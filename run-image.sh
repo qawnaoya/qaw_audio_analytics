553f03eca6ba53ef283adfb5cea38037ef11a4a9
Naoya Ikeda <nao_ikeda@qaworks.co.jp>
2019-07-30 15:37:09 +0000

    Changes to be committed:

--- run-image.sh
+++ run-image.sh
@@ -1,1 +1,1 @@
-sudo docker run --rm --name audio_analytics -v $PWD/jovyan:/home/jovyan -p 10000:8000 nikeda/audio_analytics:0.0.1
+sudo docker run --rm --name audio_analytics -v $PWD/jovyan:/home/jovyan -p 10000:8888 nikeda/audio_analytics:0.0.1
